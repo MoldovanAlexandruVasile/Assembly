ca588e62433d35eed6c61e41848a9f048a26b93b
MoldovanAlexandruVasile <moldovan_alexandru97@yahoo.com>
2016-11-04 19:57:04 +0200

    (a+b*c+2/c)/(2+a)+e with sign

---
+++ P18cs.asm
@@ -0,0 +1,67 @@
+;(a+b*c+2/c)/(2+a)+e
+;a,b-byte; c-word; e-doubleword
+;Cu semn
+
+assume DS: data, CS: code
+
+data segment
+
+	a DB 1
+	b DB 4
+	c DW 2
+	e DD 300
+
+data ends
+code segment
+
+start:
+	mov AX, data
+	mov DS, AX
+ 	
+	mov AL, b		;AL = b
+	cbw			;AX = AL
+	mul c			;DX:AX = AX * c 
+	mov CX, DX		;CX = DX
+	mov BX, AX		;BX = AX
+	;CX:BX = b * c
+	
+	mov AL, 2		;AL = 2	
+	cbw			;Converts byte to word
+	cwd			;Convers word to doubleword
+	div c			;AX = DX:AX / c
+	;AX = 2/c
+
+	cwd			;Converts word to doubleword
+	add CX, DX		;CX = CX + DX
+	add BX, AX		;BX = BX + AX
+	;CX:BX = b*c+2/c
+
+	mov AL, a		;AL = a
+	cbw			;Converts byte to word AL -> AX
+	cwd			;Convers word to doubleword AX -> DX:AX
+	add CX, DX		;CX = CX + DX
+	add BX, AX		;BX = BX + AX
+	;CX:BX = (a+b*c+2/c)
+
+	mov AL, a		;AL = a
+	add AL, 2		;AL = AL + 2
+	cbw			;AX = AL
+	mov DX, CX		;DX = CX
+	mov CX, AX		;CX = AX
+	mov AX, BX		;AX = CX
+	;DX:AX = (a+b*c+2/c)
+	;CX = (2+a) 	
+
+	div CX			;AX = DX:AX / CX
+	;AX = (a+b*c+2/c)/(2+a)
+
+	cwd			;DX:AX = AX
+	add DX, word PTR e
+	add AX, word ptr e+2
+	;DX:AX = (a+b*c+2/c)/(2+a)+e
+
+	mov AX, 4c00h
+	int 21h
+
+code ends
+end start
