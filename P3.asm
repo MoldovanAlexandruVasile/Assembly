17b5809d40c6cbc31279a7327f4c610fa5bb835d
MoldovanAlexandruVasile <moldovan_alexandru97@yahoo.com>
2016-11-19 14:19:29 +0200

    Updated

--- P3.asm
+++ P3.asm
@@ -19,43 +19,46 @@ start:
 	mov AX, data
 	mov DS, AX
  	
-	mov BX, 0 			;BX = 0 -> will be the final result
+	mov BX, 0 			;BX = 0 -> we will work in BX
 	
 	mov AX, a			;AX = 0
-	and AX, 0000000000001010b	;We keep the bits 12-14 intact
-	;AX = 0000000000001010b
+	
+	and AX, 0111000000000000b	;We isolate the bits 12-14
+	;AX = 0111000000000000b
 
-	mov CL, 4
-	ror AX, CL			;rotate to right 4 bits
-	;AX = 1010000000000000b
+	mov CL, 4			;CL = 4
+	rol AX, CL			;We rotate AX with 4 bits to left
+	;AX = 0000000000000111b
 
-	or BX, AX			;BX = AX
-	;BX = 1010000000000000b
-
+	or BX, AX			;We move the result in BX
+	;BX = 0000000000000111b
+		
 	mov AX, b			;AX = b
-	and AX, 1001100000000000b	;We keep the bits 0-5 intact
-	;AX = 1001100000000000b		
+
+	and AX, 0000000000111111b	;We isolate the bits 0-5
+	;AX = 0000000000111111b
 
 	mov CL, 3			;CL = 3
-	ror AX, CL			;We rotare to right with 3 bits, AX
-	;AX = 0001001100000000b
+	rol AX, CL			;We rotate AX with 3 bits to left
+	;AX = 0000000111111000b
 
-	or BX, AX			;We add the bits from 0-5 to BX
-	;BX = 1011001100000000b
+	or BX, AX
+	;BX = 0000000111111111b
 
 	mov AX, a			;AX = a
-	and AX, 000101110100000b	;We keep bits 3-9 intact
-	;AX = 000101110100000b
+	
+	and AX, 0000001111111000b	;We isolate the bits 3-9
+	;AX = 0000001101010000b
 
-	mov CL, 5			;CL = 5
-	ror AX, CL			;We rotate to right with 5 bits
-	;AX = 000000001011101b
+	mov CL, 6			;CL = 6
+	rol AX, CL			;We rotate AX with 6 bits to left
+	;AX = 1101010000000000b
 
-	or BX, AX			;We add the bits 9-15 to BX
-	;BX = 1011001101011101b
+	or BX, AX		
+	;BX = 1101010111111111b
 
-	mov c, BX			;c = BX
-	;c = 1011001101011101b
+	mov c, BX
+	;c = 1101010111111111b
 
 	mov AX, 4c00h
 	int 21h
